3b2560fb47da327026f876a646c2cf690e68fcc8
mrshane06 <niekerkkeegan@007gmail.com>
2024-06-04 16:24:29 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 function agetodays() {
     const age = document.getElementById('age').value;
-    const days = (age * 365);
-    document.getElementById('answer').innerHTML = days ;
+    const days = (age * 365); // multiplies age by amount of days in the year
+    document.getElementById('answer').innerHTML = days ; // displays the answer in the html
 }
